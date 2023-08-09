d8646f271896c3910e231116f2a0f19fc140904b
yuriybobyk <yuriybobyk4325@gmail.com>
2023-08-09 13:14:23 +0300

    new&popualr done

--- src/components/New&Popular.tsx
+++ src/components/New&Popular.tsx
@@ -3,27 +3,67 @@ import {useAppDispatch, useAppSelector} from "../hooks";
 import {useSearchParams} from "react-router-dom";
 import {movieActions} from "../redux";
 import {MovieCard} from "./MovieCard";
+import {Pagination, styled} from "@mui/material";
 
 
 const NewPopular = () => {
 
     const dispatch = useAppDispatch();
 
-    const {newPopularMovies, page, loading}=useAppSelector(state => state.movieReducer);
+    const {newPopularMovies, page, loading} = useAppSelector(state => state.movieReducer);
 
     const [query, setQuery] = useSearchParams();
 
     const choosenPage = query.get('page')
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(movieActions.getNewPopular({page: choosenPage}))
 
-    },[dispatch, choosenPage])
+    }, [dispatch, choosenPage])
+
+    const CustomPagination = styled(Pagination)(({theme}) => ({
+        '& .MuiPaginationItem-root': {
+            color: 'white',
+        },
+        '& .MuiPaginationItem-page.Mui-selected': {
+            backgroundColor: 'red',
+        },
+        '& .MuiPaginationItem-page.Mui-selected:hover': {
+            backgroundColor: 'darkred',
+        },
+        '& .MuiPaginationItem-page:hover': {
+            backgroundColor: 'lightcoral',
+        },
+    }));
+
 
     return (
-        <div>
-            {newPopularMovies && newPopularMovies.map(newPopular=> <MovieCard key={newPopular.id} movie={newPopular}/>) }
-        </div>
+        <main className="w-full flex items-center justify-center">
+            {loading ? <div
+                    className=" absolute top-1/3 left-1/2 inline-block h-24 w-24 animate-spin rounded-full border-4 border-solid border-red-600 border-r-transparent align-[-0.125em] text-danger motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                    role="status"><span
+                    className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
+                </div> :
+                <div className="flex w-full items-center justify-center p-4 flex-col">
+                    <section className="flex w-full  flex-wrap gap-3 top-24 items-center  mt-16">
+                        {newPopularMovies && newPopularMovies.map(newPopular => <MovieCard key={newPopular.id}
+                                                                                           movie={newPopular}/>)}
+                    </section>
+                    <div className="w-full flex justify-center">
+                        <div className="flex justify-center w-[900px] ">
+                            <CustomPagination
+                                size="large"
+                                shape="rounded"
+                                variant="text"
+                                sx={{marginY: 2}}
+                                count={500}
+                                page={+page}
+                                onChange={(_, num) => setQuery({page: `${num}`})}
+                            /></div>
+                    </div>
+                </div>
+            }
+        </main>
     );
 };
 
