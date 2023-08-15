8abded88431bedee623aaa5f761f5fd1ea7d26cb
yuriybobyk <yuriybobyk4325@gmail.com>
2023-08-15 18:28:35 +0300

    small fix

--- src/components/LeftMenu.tsx
+++ src/components/LeftMenu.tsx
@@ -27,7 +27,10 @@ const LeftMenu = ({isOpen, onClose}: IProps) => {
         <div
             className={`fixed top-0 left-0 w-64 h-full bg-[#191919] pt-8 z-10 transform transition-transform duration-700 flex flex-col  justify-center gap-4 ${menuClassName}`}>
             {genresList && genresList.map(genre => <div
-                onClick={() => navigate(`/movies/by-genre?genreId=${genre.id}&page=1`)}
+                onClick={() => {
+                    navigate(`/movies/by-genre?genreId=${genre.id}&page=1`);
+                    onClose()
+                }}
                 className="text-xl pl-8 text-left w-full cursor-pointer hover:bg-[#303030] "
                 key={genre.id}>{genre.name}</div>)}
             {isOpen && (<XIcon onClick={onClose} className="h-6 w-6 cursor-pointer absolute top-4 right-4"/>)}
