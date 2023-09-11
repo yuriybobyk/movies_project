56558984217e8d6589e2e41c1b0b4e05615fdf67
yuriybobyk <yuriybobyk4325@gmail.com>
2023-09-11 14:29:17 +0300

    small ui fix, start working on auth

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDYrSjJ91a_tzKm2LSzGKM3FxJct_n2EC8",
+    authDomain: "netflix-c8828.firebaseapp.com",
+    projectId: "netflix-c8828",
+    storageBucket: "netflix-c8828.appspot.com",
+    messagingSenderId: "267856684164",
+    appId: "1:267856684164:web:a6e648212e628d5576ced1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
