e71c55b5cea83ba50b44a01c5e2b1e4cac6d1145
yuriybobyk <yuriybobyk4325@gmail.com>
2023-09-11 14:22:10 +0300

    small ui fix

--- src/components/RowElement.tsx
+++ src/components/RowElement.tsx
@@ -14,7 +14,7 @@ const RowElement:FC<IProps> = ({movie}) => {
 
     const getPoster =`${posterURL}${poster_path}`
     return (
-        <div className="relative h-80 min-w-[200px] cursor-pointer transition duration-200 ease-out md:h-96 md:min-w-[280px] md:hover:scale-105">
+        <div className="relative h-78 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-74 md:min-w-[240px] md:hover:scale-105">
             <img className="rounded-sm object-cover  inset-0 w-full h-full md:rounded" src={getPoster} alt={title}/>
         </div>
     );
