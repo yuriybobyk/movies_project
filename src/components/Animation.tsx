d869110ea7045451ad020869eec8d7a1e6a7edff
yuriybobyk <yuriybobyk4325@gmail.com>
2023-09-27 14:48:48 +0300

    adding audio effect to animation

--- src/components/Animation.tsx
+++ src/components/Animation.tsx
@@ -1,15 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Lottie from 'lottie-react';
 import animationData from '../assets/Animation - 1695209379363.json';
+import soundFile from '../assets/soindFile.mp3';
 
 const Animation = ({ onAnimationComplete }: { onAnimationComplete: () => void }) => {
     const [animationVisible, setAnimationVisible] = useState(true);
+    const [audioStarted, setAudioStarted] = useState(false);
+    const audioRef = useRef<HTMLAudioElement | null>(null);
+
+    const handleAnimationStart = () => {
+        setAudioStarted(true);
+
+        if (audioRef.current) {
+            audioRef.current.play().catch(error => {
+                console.error('Audio play error:', error);
+            });
+        }
+    };
+
+    const handleAnimationComplete = () => {
+        onAnimationComplete();
+    };
 
     useEffect(() => {
         if (animationVisible) {
             setAnimationVisible(true);
+            handleAnimationStart();
         } else {
-            onAnimationComplete();
+            handleAnimationComplete();
         }
     }, [animationVisible, onAnimationComplete]);
 
@@ -24,9 +42,14 @@ const Animation = ({ onAnimationComplete }: { onAnimationComplete: () => void })
                     height={animationHeight}
                     width={animationWidth}
                     loop={false}
-                    onComplete={() => setAnimationVisible(false)}
+                    onComplete={() => {
+                        setAnimationVisible(false);
+                    }}
                 />
             </div>
+            <audio ref={audioRef}>
+                <source src={soundFile} type="audio/mpeg" />
+            </audio>
         </div>
     );
 };
