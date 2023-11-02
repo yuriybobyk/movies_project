0d66e2bf883df9c95ec65043a33208e4247f6158
yuriybobyk <yuriybobyk4325@gmail.com>
2023-11-02 13:47:08 +0200

    some changes

--- src/components/MovieModal.tsx
+++ src/components/MovieModal.tsx
@@ -80,12 +80,20 @@ const MovieModal: React.FC<MovieModalProps> = ({onClose, trailer}) => {
                             <p className="font-light">{modalMovie?.release_date || modalMovie.first_air_date}</p>
                             <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">HD</div>
                         </div>
-                        <div>
+                        <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
                             <p className="w-5/6">{modalMovie?.overview}</p>
                             <div className="flex flex-col space-y-3 text-sm">
                             <div>
                                 <span className="text-[gray]">Genres: </span>
                                 {modalMovie.genres.map((genre)=>genre.name).join(', ')}
+                            </div>
+                            <div>
+                                <span className="text-[gray]">Original language: </span>
+                                {modalMovie?.original_language}
+                            </div>
+                            <div>
+                                <span className="text-[gray]">Total votes: </span>
+                                {modalMovie?.vote_count}
                             </div>
                             </div>
                         </div>
